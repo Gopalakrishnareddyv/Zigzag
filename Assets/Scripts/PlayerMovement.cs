fd79075d73d40e55bfeef9ace3519f1bbaed9fb0
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-05-26 15:06:18 +0530

    playermoved

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 Direction;
+
+    public float playerspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Direction == Vector3.forward)
+            {
+                Direction = Vector3.left;
+            }
+            else
+            {
+                Direction = Vector3.forward;
+            }
+        }
+        transform.Translate(Direction * playerspeed * Time.deltaTime);
+    }
+}
