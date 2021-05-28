0e1b052e1e45a1e97d0ab3760c40850f42ff0374
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-05-28 18:00:39 +0530

    created stack to spawn tiles

--- Assets/Scripts/TileManagerScript.cs
+++ Assets/Scripts/TileManagerScript.cs
@@ -6,6 +6,9 @@ public class TileManagerScript : MonoBehaviour
 {
     public GameObject[] tiles;
     public GameObject currenttile;
+    Stack<GameObject> farwardTilePool = new Stack<GameObject>();
+    Stack<GameObject> leftTilePool = new Stack<GameObject>();
+
     // Start is called before the first frame update
     private static TileManagerScript instance;
 
@@ -23,7 +26,7 @@ public class TileManagerScript : MonoBehaviour
 
     void Start()
     {
-        
+        CreateTiles(5);
     }
 
     // Update is called once per frame
@@ -31,10 +34,39 @@ public class TileManagerScript : MonoBehaviour
     {
         
     }
+    void CreateTiles(int value)
+    {
+        for (int i=0; i < value; i++)
+        {
+            farwardTilePool.Push(Instantiate(tiles[0]));
+            leftTilePool.Push(Instantiate(tiles[1]));
+            farwardTilePool.Peek().SetActive(false);
+            leftTilePool.Peek().SetActive(false);
+        }
+        
+    }
     public void TileSpawn()
     {
+        if (farwardTilePool.Count==0 || leftTilePool.Count == 0)
+        {
+            CreateTiles(5);
+        }
         int index = Random.Range(0, tiles.Length);
-        currenttile = (GameObject)Instantiate(tiles[index], currenttile.transform.GetChild(index).position , Quaternion.identity);
+        if (index == 0)
+        {
+            GameObject temp = farwardTilePool.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currenttile.transform.GetChild(index).position;
+            currenttile = temp;
+        }
+        else if (index == 1)
+        {
+            GameObject temp = leftTilePool.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currenttile.transform.GetChild(index).position;
+            currenttile = temp;
+        }
+        
 
     }
 }
--- Assets/Scripts/TilesScrpt.cs
+++ Assets/Scripts/TilesScrpt.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TilesScrpt : MonoBehaviour
 {
+    //Rigidbody tofall;
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
@@ -19,10 +21,24 @@ public class TilesScrpt : MonoBehaviour
     {
         if (other.tag=="player")
         {
-            for (int i = 0; i < 5; i++)
+            for(int i = 0; i < 5; i++)
             {
                 TileManagerScript.Instance.TileSpawn();
+                
+                
+                //Rigidbody tofall = GameObject.FindGameObjectWithTag("tile").AddComponent<Rigidbody>();
+
             }
+            StartCoroutine("TileFall");
+
+
         }
     }
+    IEnumerable TileFall()
+    {
+        yield return new WaitForSeconds(5);
+        print("Waited");
+        //tofall.useGravity = true;
+    }
+    
 }
