d0601b29ac5685c5591991c1911a1770b26b1811
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-06-02 09:34:12 +0530

    WinUi and RestartUI with animatons are applied

---
+++ Assets/Scripts/PlayerDeadScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeadScript : MonoBehaviour
+{
+    public GameObject deathui;
+    PlayerMovement playermove;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playermove = GetComponent<PlayerMovement>();
+        /*if (transform.position.y != 1.0f)
+        {
+            print("Game Over");
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(transform.position.y);
+        if (transform.position.y== 1.0f)
+        {
+            //print("Game Over");
+
+            
+            
+        }
+        else
+        {
+            deathui.SetActive(true);
+            transform.position = new Vector3(-1.5f, 1, -2);
+            playermove.playerspeed = 0;
+        }
+        
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     Vector3 Direction;
 
+    public GameObject winui;
+    PlayerMovement playermove;
+
     public float playerspeed;
     public GameObject particleprefab;
+    [SerializeField]
+    int score = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        playermove = GetComponent<PlayerMovement>();
     }
-    
+
 
     // Update is called once per frame
     void Update()
@@ -28,16 +33,30 @@ public class PlayerMovement : MonoBehaviour
             else
             {
                 Direction = Vector3.forward;
+
             }
+            score = score + 1;
         }
         transform.Translate(Direction * playerspeed * Time.deltaTime);
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "coin")
         {
+            score = score + 10;
+            print(score);
             other.gameObject.SetActive(false);
             Instantiate(particleprefab, transform.position, Quaternion.identity);
+            if (score >=20)
+            {
+                winui.SetActive(true);
+                transform.position = new Vector3(-1.5f, 1, -2);
+                playermove.playerspeed = 0;
+            }
         }
     }
+   
+
+
 }
